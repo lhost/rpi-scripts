de407bb24f34552d5581ade3f2aeee9cac6fcd7f
Lubomir Host <lubomir.host@gmail.com>
2019-11-14 23:17:28 +0100

    Install development tools. Raspberry PI detection

---
+++ install-devel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+KERNEL_HEADERS=linux-headers-amd64
+
+# detect Raspberry Pi
+if [ -f /proc/device-tree/model ]; then
+	grep -q -i Raspberry
+	if [ $? -eq 0 ]; then
+		KERNEL_HEADERS=raspberrypi-kernel-headers
+	fi
+fi
+
+apt-get install -y \
+	debhelper \
+	dh-autoreconf \
+	dh-exec \
+	dh-make \
+	dh-strip-nondeterminism \
+	build-essential \
+	gdb libtool \
+	dkms module-assistant \
+	dpkg-dev \
+	$KERNEL_HEADERS
+
+exit 0
+
+# vim: fdm=marker fdl=0 fdc=3
