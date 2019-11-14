61da06dc9ac7684681428401ea0bc4ce2906b22f
Lubomir Host <lubomir.host@gmail.com>
2019-11-14 23:53:33 +0100

    Next packages for image processing

--- image-recognition-install.sh
+++ image-recognition-install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-apt-get install -y \
-	python3-pip
+# https://www.pyimagesearch.com/2015/11/30/detecting-machine-readable-zones-in-passport-images/
+# https://pillow.readthedocs.io/en/latest/installation.html
 
-for pythonpkg in tensorflow numpy scipy opencv-python pillow matplotlib h5py keras imageai ; do
+apt-get install -y \
+	python3-pip \
+	python3-dev \
+	python3-setuptools \
+	tesseract-ocr \
+	gimagereader \
+	libtesseract-dev \
+	libjpeg9-dev \
+	zlib1g-dev \
+	libtiff5-dev \
+	python-sphinx python-sphinx-rtd-theme python-sphinxcontrib.spelling python-mock python-pil python-opencv python-wand
+
+
+for pythonpkg in PassportEye tensorflow numpy scipy opencv-python pillow matplotlib h5py keras imageai imutils ; do
 	echo -------------------- $pythonpkg ------------;
 	pip3 install $pythonpkg --upgrade;
 done
