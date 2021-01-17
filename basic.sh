8934bb93e6de797463e34b55d9059dcf9d8b6695
Lubomir Host <lubomir.host@gmail.com>
2021-01-17 21:41:09 +0100

    Run upgrade first

--- basic.sh
+++ basic.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # vim: ft=sh fdm=marker
 
+apt-get update
+apt-get dist-upgrade
+
 sudo apt-get install -y \
 	git etckeeper screen mc vim ssh liquidprompt \
 	deborphan \
 	rename \
 	lm-sensors i2c-tools
-
 
 # liquidprompt
 liquidprompt_activate
